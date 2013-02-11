8b11bd6ce395a7fda297ac807e322bab38a712cd
idmpotent <venu.alla@berkeley.edu>
2013-02-11 08:38:24 -0800

    added idmatch-custom-config
    place this file in a external directory that is as configured in
    Config.groovy

---
+++ grails-app/controllers/dolphin/UserController.groovy
@@ -0,0 +1,5 @@
+package dolphin
+
+class UserController {
+    static  scaffold = true;
+}
---
+++ grails-app/domain/dolphin/User.groovy
@@ -0,0 +1,12 @@
+package dolphin
+
+class User {
+
+    static constraints = {
+    }
+    
+    String attr1 //ssn
+    String attr2 //fname
+    String attr3 //lname
+    String attr4 //dob
+}
